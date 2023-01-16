c9753e7457f94f2367a61c66cc9e44986f1bae05
arianrdcas <arianrdcas@gmail.com>
2023-01-16 17:45:43 -0300

    mejorando navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,39 +1,39 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Navbar = () => {
   const location = useLocation();
 
   return (
     <div>
-      <nav className="navbar navbar-dark navbar-expand-lg bg-dark ">
+      <nav className="navbar navbar-dark  navbar-expand-lg bg-dark ">
         <div className="container-fluid">
           <div
             className="collapse navbar-collapse justify-content-center"
             id="navbarSupportedContent"
           >
             <ul className="nav ">
-              <li className="nav-item">
-                <Link
+              <li className="nav-item ">
+                <NavLink
                   className={
-                    "nav-link text-white" +
+                    "nav-link text-white " +
                     (location.pathname === "/" ? "active" : "")
                   }
                   to="/"
                 >
                   Bienvenido
-                </Link>
+                </NavLink>
               </li>
-              <li className="nav-item">
-                <Link
+              <li className="nav-item ">
+                <NavLink
                   className={
-                    "nav-link  text-white" +
+                    "nav-link text-white " +
                     (location.pathname === "/postulaciones" ? "active" : "")
                   }
                   to="/postulaciones"
                 >
                   Postulaciones
-                </Link>
+                </NavLink>
               </li>
             </ul>
           </div>
